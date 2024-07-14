6d96f339738950f1bacfa237c675df082ee68c70
amitsuthar803 <amitsuthar803@gmail.com>
2024-07-14 14:31:47 +0530

    first commit

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const app = express();
 require("./db/conn");
 const cors = require("cors");
 const router = require("./Routes/router");
-const PORT = process.env.PORT || 6010
-
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads",express.static("./uploads"));
-app.use("/files",express.static("./public/files"));
+app.use("/uploads", express.static("./uploads"));
+app.use("/files", express.static("./public/files"));
 
 app.use(router);
 
-app.listen(PORT,()=>{
-    console.log(`Server start at port no ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server start at port no ${PORT}`);
+});
