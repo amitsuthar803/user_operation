cd599ad5fc237a73677825fb1919d27b0c2764a9
amitsuthar803 <amitsuthar803@gmail.com>
2024-07-14 14:22:47 +0530

    first commit

---
+++ db/conn.js
@@ -0,0 +1,14 @@
+require("dotenv").config(); // Load environment variables from .env file
+const mongoose = require("mongoose");
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DataBase Connected"))
+  .catch((err) => {
+    console.log(err);
+  });
